22c81fe96761aabece66f18b88944d5d6b666e7e
gerardng <gerardngjr@yahoo.com>
2016-09-10 09:37:43 -0700

    added students

--- Student.php
+++ Student.php
@@ -19,13 +19,17 @@ class Student
         $this->emails[$which] = $address;
     }
 
-    /*If you assign values to an array without the index,
+    /* NOTE: If you assign values to an array without the index,
     PHP will automatically increment the internal reference
     so each value goes into the next element. */
+
+    /* parameter: $grade to be added.
+    Adds another grade to array of $grades[] inside this student */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    /* For each value in $grades[] array, add to $total and compute average */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -33,16 +37,17 @@ class Student
         return $total / count($this->grades);
     }
 
-    /* When traversing an array of key/value pairs,
-    for instance "foreach($stuff as $key =>> $value)", you use the => operator to tell PHP to
-    put the current key into the $key variable and
-    the associated value at the key into the $value variable */
+    /* Returns first name, last name, average, each email in a string */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
         foreach($this->emails as $which=>$what)
             $result .= $which . ': '. $what. "\n";
-        $result .= "\n";
         return '<pre>'.$result.'</pre>';
     }
+
+    /* NOTE: When traversing an array of key/value pairs,
+    for instance "foreach($stuff as $key =>> $value)", you use the => operator to tell PHP to
+    put the current key into the $key variable and
+    the associated value at the key into the $value variable */
 }
--- index.php
+++ index.php
@@ -1,44 +1,67 @@
-
-
 <!DOCTYPE html>
-
-
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Comp 4711 Lab 1 by Gerard Ng</title>
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
     <body>
-        <?php
-            include('student.php');
+        <div class="container">
+            <br>
+            <div class="jumbotron">
+                <h1><i class=" glyphicon glyphicon-list-alt"></i> BCIT Student Records</h1>
+                <p>This is personal information. By reading this, you are agreeing to a non-disclosure contract.
+                    You may not use any information gathered here for commercial use.</p>
+                <p><a class="btn btn-primary btn-lg" href="#" role="button">Students <span class="badge">3</span></a></p>
+            </div>
+            <hr>
+            <h3><?php
+                include('student.php');
 
-            $students = array();
+                $students = array();
+                /* added first student */
+                $first = new Student();
+                $first->surname = "Doe";
+                $first->first_name = "John";
+                $first->add_email('home','john@doe.com');
+                $first->add_email('work','jdoe@mcdonalds.com');
+                $first->add_grade(65);
+                $first->add_grade(75);
+                $first->add_grade(55);
+                $students['j123'] = $first;
 
-            /* added first student */
-            $first = new Student();
-            $first->surname = "Doe";
-            $first->first_name = "John";
-            $first->add_email('home','john@doe.com');
-            $first->add_email('work','jdoe@mcdonalds.com');
-            $first->add_grade(65);
-            $first->add_grade(75);
-            $first->add_grade(55);
-            $students['j123'] = $first;
+                /* added second student */
+                $second = new Student();
+                $second->surname = "Einstein";
+                $second->first_name = "Albert";
+                $second->add_email('home','albert@braniacs.com');
+                $second->add_email('work1','a_einstein@bcit.ca');
+                $second->add_email('work2','albert@physics.mit.edu');
+                $second->add_grade(95);
+                $second->add_grade(80);
+                $second->add_grade(50);
+                $students['a456'] = $second;
 
-            /* added second student */
-            $second = new Student();
-            $second->surname = "Einstein";
-            $second->first_name = "Albert";
-            $second->add_email('home','albert@braniacs.com');
-            $second->add_email('work1','a_einstein@bcit.ca');
-            $second->add_email('work2','albert@physics.mit.edu');
-            $second->add_grade(95);
-            $second->add_grade(80);
-            $second->add_grade(50);
-            $students['a456'] = $second;
+                /* added myself as a student */
+                $third = new Student();
+                $third->surname = "Ng";
+                $third->first_name = "Gerard";
+                $third->add_email('home','gerard@gerardng.com');
+                $third->add_email('school','gerard@bcit.ca');
+                $third->add_grade(99);
+                $third->add_grade(98);
+                $third->add_grade(99);
+                $third->add_grade(100);
+                $third->add_grade(98);
+                $students['a008'] = $third;
 
-            foreach($students as $student)
-                echo $student->toString();
-        ?>
+                ksort($students);	// one of the many sort functions
+
+                /* for each $student object in $students array, print out $student's toString method */
+                foreach($students as $student)
+                        echo $student->toString() . '<hr>';
+            ?></h3>
+        </div>
     </body>
 </html>
